d5dbc2617912081cb33ee99b079adb8130285158
hellboy8171 <73305213+hellboy8171@users.noreply.github.com>
2022-03-22 14:58:15 +0530

    Update repo.py

--- git_repo/files/repo.py
+++ git_repo/files/repo.py
@@ -1,5 +1,5 @@
 from github import Github
 
-g = Github("hellboy8171", "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")
+g = Github("hellboy8171", "ghp_qoPEWtNSJnTRRvnT5IRNrSh0qJDYIvP6R")
 user = g.get_user()
 repo = user.create_repo('gam')
